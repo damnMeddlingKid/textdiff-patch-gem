8f44b1b54cde50065243f4ffb566abea013c1623
Franklyn D'souza <franklynd@gmail.com>
2016-02-09 21:15:21 -0500

    patch method

--- lib/text_diff_patch.rb
+++ lib/text_diff_patch.rb
@@ -1,4 +1,23 @@
 module TextDiffPatch
-	def TextDiffPatch.patch
+	def TextDiffPatch.patch(old, delta)
+    result = ''
+    index = 0
+
+    delta.each do |item|
+      operation = item[0]
+      value = item[1]
+
+      case operation
+      when -1
+        index++
+      when 0
+        result += old[index,index+=value]
+      when 1
+        result += value
+      end
+      
+    end
+
+    result
 	end
 end
